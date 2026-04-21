789c4422d80e6e921190fbcccfd7e65cc568b29a
Joel Bermudez <jbermudezacosta@technisys.com>
2026-04-20 20:31:16 -0500

    Initial Commit

---
+++ Lyrics/LyricsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LyricsApp.swift
+//  Lyrics
+//
+//  Created by Joel Bermudez on 20/4/26.
+//
+
+import SwiftUI
+
+@main
+struct LyricsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
